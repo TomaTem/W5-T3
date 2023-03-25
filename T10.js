3c5788eb144f3c8543225f46f31fcd82c5b16904
TomaTem <110160066+TomaTem@users.noreply.github.com>
2023-03-25 11:17:13 +0300

    Add files via upload

--- T10.js
+++ T10.js
@@ -5,12 +5,13 @@
 // ([1, 2], [2, 3]) => [2]
 // (['a', 'b'], ['b', 'c'], ['b', 'e', 'c']) => ['b']
 
+// 1 способ
 function commonArr(arr) {
     let repeats = [];
     let result = [];
     for (let i=0; i<arr.length-1; i++) {
         for (let j=0; j<=arr[i].length; j++) {     
-            if (arr[i].includes(arr[i+1][j])) {
+            if (arr[i].includes(arr[i+1][j]) && arr[i+1].indexOf(arr[i+1][j]) == j) {
                 repeats.push(arr[i+1][j]);
             }
         };
@@ -28,7 +29,33 @@ function commonArr(arr) {
     return result;
 }
 
+// 2 способ
+function commonArr2(arr) {
+    let obj = arr[0].reduce((acc, el) => {
+        acc[el] = 1;
+        return acc;
+    }, {});
+    for (let i=1; i<arr.length; i++) {
+        for (let j=0; j<=arr[i].length; j++) {
+            for (let key in obj) {
+                if (key == arr[i][j] && arr[i].indexOf(arr[i][j]) == j) {
+                obj[key] += 1;         
+            } 
+    }}}
+    let result = [];
+    for (let key in obj) {
+        if (obj[key] == (arr.length)) {
+            if (Number(key) == key) result.push(+key);
+            else result.push(key);
+        } 
+    }
+    return result;
+}
+
 let arr1 = [['a', 'b', 4], [4, 'b', 'c'], ['b', 4, 'e'], ['b', 'e', 4]];
 let arr2 = [[1, 2], [2, 3]];
 console.log(commonArr(arr1));
 console.log(commonArr(arr2));
+
+console.log(commonArr2(arr1));
+console.log(commonArr2(arr2));
